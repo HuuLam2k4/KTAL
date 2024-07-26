9bfd991a03c72a3e0f7f68764db1fcd3410f6c55
thuann17 <117891819+thuann17@users.noreply.github.com>
2024-07-26 16:30:15 +0700

    T
    T

---
+++ src/main/java/com/poly/websitegaminggear/WebsiteGamingGearApplication.java
@@ -0,0 +1,13 @@
+package com.poly.websitegaminggear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteGamingGearApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsiteGamingGearApplication.class, args);
+    }
+
+}
